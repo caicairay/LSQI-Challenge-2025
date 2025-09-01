411ed8401b2f8a324b5adf2b9c45cb270b1056e8
Zhuo <z.cao@fz-juelich.de>
2025-09-01 17:15:54 +0200

    visualization

--- data/visualization.py
+++ data/visualization.py
@@ -2,34 +2,30 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+sns.set(style="whitegrid")
 # Load data
 data = pd.read_csv("./EstData.csv")
+fig, axs = plt.subplots(6, 8, figsize= (20, 15), sharex = True, sharey=True)
+for id, ax in enumerate(axs.flatten()):
+    data_plot = data[data["ID"] == (id+1)]
+    # Set style
+    sns.lineplot(
+        data=data_plot,
+        x="TIME",
+        y="DV",
+        style="EVID",  # distinguish by DOSE
+        hue="DVID",        # distinguish by ID
+        markers=True,
+        dashes=False,
+        palette="tab10",
+        ax=ax,
+        legend='brief' if (id == 47 or id == 11) else False
+    )
 
-# Filter for DOSE
-# data = data[data["DOSE"] == 1]
-# Filter for DVID
-data = data[data["DVID"] == 1]
-data = data[data["CMT"] == 2]
-
-# Set style
-sns.set(style="whitegrid")
-
-# Create plot
-plt.figure(figsize=(12, 6))
-sns.lineplot(
-    data=data,
-    x="TIME",
-    y="DV",
-    hue="DOSE",  # distinguish by DOSE
-    # hue="ID",        # distinguish by ID
-    markers=True,
-    dashes=False,
-    palette="tab10",
-)
-
-plt.title("TIME vs DV by DOSE and ID")
+#plt.title("TIME vs DV by DOSE and ID")
 plt.xlabel("TIME")
 plt.ylabel("DV")
-plt.legend(title="DOSE / ID", bbox_to_anchor=(1.05, 1), loc="upper left")
+# plt.legend(title="DOSE / ID", bbox_to_anchor=(1.05, 1), loc="upper left")
 plt.tight_layout()
-plt.show()
+#plt.show()
+plt.savefig('test.png')
